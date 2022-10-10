0aedf00e0166b4503c19342a4d95f04569990ca1
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-10-10 17:56:35 +0200

    setting navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,22 @@ import {Container} from "react-bootstrap"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Store from "./pages/Store"
+import Navbar from "./components/Navbar"
 
 
 function App() {
-  
-
-  return <Container>
+  return (
+  <>
+  <Navbar/>
+  <Container className="mb-4 mt-4">
     <Routes>
       < Route path="/" element={<Home/>}/>
       < Route path="/about" element={<About/>}/>
       < Route path="/store" element={<Store/>}/>
     </Routes>
   </Container>
+  </>
+  )
 }
 
 export default App
