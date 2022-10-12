30f0e072d4cbc3df452a5f5e36ec2f266c88f63f
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-10-12 16:42:51 +0200

    Nav link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { Navbar as NavbarBs } from "react-bootstrap";
+import { Nav, Navbar as NavbarBs } from "react-bootstrap";
 import {Container} from "react-bootstrap"
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <NavbarBs className="bg-white shadow-sm mb-3">
-      <Container>nav</Container>
+      <Container>
+        <Nav>
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+        </Nav>
+        </Container>
     </NavbarBs>
   );
 };
