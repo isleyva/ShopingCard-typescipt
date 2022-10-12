c6ccc921e8e888419b563090bd37a1226e7e97cb
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-10-12 18:17:32 +0200

    typing each prop

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+type StoreItemProps = {
+    id:number
+    name:string
+    price:number
+    imgUrl:string
+}
+
 const StoreItem = ({id, name, price, imgUrl}) => {
   return (
     <div>StoreItem</div>
