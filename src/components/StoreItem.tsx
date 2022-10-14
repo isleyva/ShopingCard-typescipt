3c8a1072e054a89f63a047aa15ff8518681fda49
Ivan Sanchez Leyva <deepman42@protonmail.com>
2022-10-14 13:13:56 +0200

    card title

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
 
 type StoreItemProps = {
-    id:number
-    name:string
-    price:number
-    imgUrl:string
-}
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
 
-const StoreItem = ({id, name, price, imgUrl} : StoreItemProps) => {
+const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
   return (
     <Card>
-        <Card.Img variant="top" src={imgUrl} height="200px" style={{
-            objectFit: "cover"
-        }}/>
-    </Card>     
-    )
-}
+      <Card.Img
+        variant="top"
+        src={imgUrl}
+        height="200px"
+        style={{
+          objectFit: "cover",
+        }}
+      />
+      <Card.Body className="d-flex flex-column" >
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+        <span className="fs-2">{name}</span>
+        <span className="ms-2">{price}</span>
+        </Card.Title>
+      </Card.Body>
+    </Card>
+  );
+};
 
-export default StoreItem
+export default StoreItem;
